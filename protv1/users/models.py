caf092a1ea2df3cc0b056a3713a0f2c18f9e285e
Aaranya Prasad <f20190107@goa.bits-pilani.ac.in>
2021-10-01 11:47:11 +0530

    Add Certificate

--- protv1/users/models.py
+++ protv1/users/models.py
@@ -5,3 +5,9 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
