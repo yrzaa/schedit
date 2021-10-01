dc7ef6962e2c46d1f22d5f07bfe1dded622812d6
Aaranya Prasad <f20190107@goa.bits-pilani.ac.in>
2021-10-01 12:03:06 +0530

    Add TimeSlot

--- protv1/users/models.py
+++ protv1/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 class CustomUser(AbstractUser):
     pass
@@ -11,3 +12,9 @@ class Certificate(models.Model):
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
+class TimeSlot(models.Model):
+    startTime = models.DateTimeField(blank=False, null=false)
+    endTime = models.DateTimeField(blank=False, null= False)
+
+    certificate = ForeignKey(Certificate, on_delete=models.PROTECT,)
+    
