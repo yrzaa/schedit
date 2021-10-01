bc136a7625932446571ce63e60f279f9c48d2529
Aaranya Prasad <f20190107@goa.bits-pilani.ac.in>
2021-10-01 12:43:44 +0530

    Add Request

--- protv1/users/models.py
+++ protv1/users/models.py
@@ -1,20 +1,42 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import BooleanField
 from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 class CustomUser(AbstractUser):
     pass
 
 class Certificate(models.Model):
-    name = models.CharField(max_length=255)
+    '''Model representing a Certificate'''
+    name = models.CharField(max_length=255, blank=False)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
 class TimeSlot(models.Model):
-    startTime = models.DateTimeField(blank=False, null=false)
-    endTime = models.DateTimeField(blank=False, null= False)
+    '''Model representing a Timeslot'''
+    startTime = models.DateTimeField(blank=False, null=True)
+    endTime = models.DateTimeField(blank=False, null= True)
 
-    certificate = ForeignKey(Certificate, on_delete=models.PROTECT,)
-    
+    #certificate = ForeignKey(Certificate, on_delete=models.PROTECT,)
+
+class Application(models.Model):
+    '''Model representing a request for a certificate'''
+    user = ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.PROTECT, blank= False)
+
+    PROCESSING_STATUS = (
+        ('a','Accepted'),
+        ('r','Rejected'),
+        ('w','Waiting'),
+    )
+    status= models.CharField(max_length=1,choices=PROCESSING_STATUS, blank=False, default='w')
+
+    collected = models.BooleanField(default=False)
+    delayed = models.BooleanField(default=False)
+
+    certificate = ForeignKey(Certificate, on_delete=models.PROTECT, blank=False)
+    slot = ForeignKey(TimeSlot ,on_delete=models.PROTECT, blank=False)
+
+
