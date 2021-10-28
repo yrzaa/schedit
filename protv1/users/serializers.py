554ed6e04bdbddb0b9dc06058b8289b86f3da94a
Aaranya Prasad <f20190107@goa.bits-pilani.ac.in>
2021-10-28 20:20:19 +0530

    Add requests to user details

--- protv1/users/serializers.py
+++ protv1/users/serializers.py
@@ -1,12 +1,19 @@
+from django.db.models import fields
 from rest_framework import serializers
 from users import models
 
+class ApplicationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Application
+        fields = ('certificate', 'slot', 'status', 'collected', 'delayed',)
+
 class CustomUserSerializer(serializers.ModelSerializer):
     '''serializes a CustomUser profile object'''
-
+    application_set = ApplicationsSerializer(many=True, read_only=True)
+    
     class Meta:
         model = models.CustomUser
-        fields = ('id', 'email', 'name', 'password')
+        fields = ('id', 'email', 'name', 'application_set', 'password')
         extra_kwargs = {
             'password': {
                 'write_only': True,
@@ -22,4 +29,6 @@ class CustomUserSerializer(serializers.ModelSerializer):
             password = validated_data['password']
         )
         return user
+
+
 
